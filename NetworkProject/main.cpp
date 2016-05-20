0157463a7dbeee434c9c09b81ec92827820b5c5c
jjeong <yjchoi0606@postech.ac.kr>
2016-05-21 02:43:42 +0900

    client command design

--- NetworkProject/main.cpp
+++ NetworkProject/main.cpp
@@ -6,16 +6,43 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <winsock2.h>
+#include <windows.h>
 
 #include "shader.h"
 #include "timer.h"
+#include "server.h"
 #include "key.h"
+#include "protocol.h"
+
+
+
+SOCKET hServSock;			// of server
+SOCKET hClntSock[6];		// of server
+SOCKET hSocket;				// of client
+SOCKADDR_IN servAddr;		// of server/client
+SOCKADDR_IN clntAddr[6];	// of server
+int szClntAddr[6];			// of server
+char messageToClient[];		// of server
+char messageFromClient[][sizeof(protocol_data) + 1]; // of server
+char messageToServer[];		// of client
+int mode;					//determine server(1) client(2) or nothing (0)
+int characterSelection;		//character_selection_time;
+int gameStart;				// start(1)
+
+
 
 void main(int argc, char **argv)
 {
 	srand(time(NULL));
+
+	mode = 0;
+	characterSelection = 0;
+	gameStart = 0;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -25,10 +52,19 @@ void main(int argc, char **argv)
 
 	glewInit();
 
-	Key::init();
 	Shader::init();
 	//60fps, 0.5 second per turn
 	Timer::init(16, 30);
+	Key::init();
+
+	/*if (argc == 2) mode = 1;
+	else if (argc == 3) mode = 2;
+	else
+	{
+		printf("Server Usage : %s <port> \n", argv[0]);
+		printf("Client Usage : %s <ip> <port>\n", argv[0]);
+		exit(1);
+	}*/
 
 	glutMainLoop();
 }
--- NetworkProject/timer.cpp
+++ NetworkProject/timer.cpp
@@ -9,10 +9,14 @@
 #include <cstdio>
 
 #include "timer.h"
+#include "protocol.h"
 #include "key.h"
 
 int Timer::frameInterval;
 int Timer::framePerTurn;
+extern int mode;
+extern int characterSelection;
+int command;
 
 void Timer::init(int interval, int perturn) {
 	frameInterval = interval;
@@ -23,7 +27,10 @@ void Timer::init(int interval, int perturn) {
 
 void Timer::turn() {
 	std::cout << "Turn!" << std::endl;
-	printf("¹¹¾ß");
+	if (mode > 0 && characterSelection > 0)
+	{
+
+	}
 	// add any per turn action
 	
 
@@ -32,18 +39,101 @@ void Timer::turn() {
 void Timer::update(int count) {
 	glutTimerFunc(frameInterval, update, count + 1);
 
-	if (Key::keyCheckOn('w')) printf("w was clicked");
-	//else printf("%d", count);
-
 	if (count % framePerTurn == 0) {
 		turn();
 	}
 
-	//std::cout << "Frame number " << count << std::endl;
-	
+	if (mode > 0) printf("mode CHANGED!");
+
 	// add any per frame actions
 	// such as update() and draw() for all object
-	if (Key::keyCheckPressed('a')) printf("hihi");
+
+	if (mode == 0 && Key::keyCheckPressed('1'))
+	{
+		mode = 1;
+	}
+	if (mode == 0 && Key::keyCheckPressed('2'))
+	{
+		mode = 2;
+	}// if not 1 or 2 give error message
+	
+
+	//if (characterSelection == 0 && Key::keyCheckPressed('a') && Key::keyCheckOn('s')) printf("hihi"); // sa
+	
+		
+	if (mode > 0)
+	{
+		if (characterSelection == 0 && Key::keyCheckPressed('1'))
+		{
+			characterSelection = COMMAND_SPAWN_CSE;
+		}
+		if (characterSelection == 0 && Key::keyCheckPressed('2'))
+		{
+			characterSelection = COMMAND_SPAWN_PHYS;
+		}
+		if (characterSelection == 0 && Key::keyCheckPressed('3'))
+		{
+			characterSelection = COMMAND_SPAWN_LIFE;
+		}
+		if (characterSelection == 0 && Key::keyCheckPressed('4'))
+		{
+			characterSelection = COMMAND_SPAWN_ME;
+		}
+		if (characterSelection == 0 && Key::keyCheckPressed('5'))
+		{
+			characterSelection = COMMAND_SPAWN_CHEM;
+		}
+		if (characterSelection > 0)
+		{
+			if (Key::keyCheckPressed('d')) command = COMMAND_MOVE_RIGHT;
+			if (Key::keyCheckPressed('w')) command = COMMAND_MOVE_UP;
+			if (Key::keyCheckPressed('a')) command = COMMAND_MOVE_LEFT;
+			if (Key::keyCheckPressed('s')) command = COMMAND_MOVE_DOWN;
+
+			if (Key::keyCheckPressed('d') && Key::keyCheckOn('j')) command = COMMAND_ATTACK_RIGHT;
+			if (Key::keyCheckPressed('w') && Key::keyCheckOn('j')) command = COMMAND_ATTACK_UP;
+			if (Key::keyCheckPressed('a') && Key::keyCheckOn('j')) command = COMMAND_ATTACK_LEFT;
+			if (Key::keyCheckPressed('s') && Key::keyCheckOn('j')) command = COMMAND_ATTACK_DOWN;
+
+			if (Key::keyCheckPressed('d') && Key::keyCheckOn('k')) command = COMMAND_SKILL_RIGHT;
+			if (Key::keyCheckPressed('w') && Key::keyCheckOn('k')) command = COMMAND_SKILL_UP;
+			if (Key::keyCheckPressed('a') && Key::keyCheckOn('k')) command = COMMAND_SKILL_LEFT;
+			if (Key::keyCheckPressed('s') && Key::keyCheckOn('k')) command = COMMAND_SKILL_DOWN;
+
+			if (Key::keyCheckPressed('l')) command = COMMAND_FLAG;
+		}
+
+	}
+
+	if (Key::keyCheckOn('w')) printf("w was clicked");
+
+
+	/*
+	
+	COMMAND_MOVE_RIGHT,
+	COMMAND_MOVE_UP,
+	COMMAND_MOVE_LEFT,
+	COMMAND_MOVE_DOWN,
+	
+	COMMAND_ATTACK_RIGHT,
+	COMMAND_ATTACK_UP,
+	COMMAND_ATTACK_LEFT,
+	COMMAND_ATTACK_DOWN,
+	
+	COMMAND_SKILL_RIGHT,
+	COMMAND_SKILL_UP,
+	COMMAND_SKILL_LEFT,
+	COMMAND_SKILL_DOWN,
+	
+	COMMAND_SPAWN_CSE,
+	COMMAND_SPAWN_PHYS,
+	COMMAND_SPAWN_LIFE,
+	COMMAND_SPAWN_ME,
+	COMMAND_SPAWN_CHEM,
+	
+	COMMAND_FLAG
+	
+	*/
 
 	Key::keyUpdate();
 }
