fbbb4a351c731d68b52c12dd6feace7349dac599
jjeong <yjchoi0606@postech.ac.kr>
2016-05-21 19:43:34 +0900

    네트워크안정화/객체생성

--- NetworkProject/main.cpp
+++ NetworkProject/main.cpp
@@ -18,36 +18,38 @@
 #include "server.h"
 #include "key.h"
 #include "protocol.h"
+#include "unit.h"
 
 
+SOCKET hServSock;												// of server
+SOCKET hClntSock[UNIT_NUM_MAX];									// of server
+SOCKET hSocket;													// of client
+SOCKADDR_IN servAddr;											// of server/client
+SOCKADDR_IN clntAddr[UNIT_NUM_MAX];								// of server
+int szClntAddr[UNIT_NUM_MAX];									// of server
+char messageToClient[MESSAGE_T0_CLIENT_SIZE];					// of server
+char messageFromClient[UNIT_NUM_MAX][MESSAGE_TO_SERVER_SIZE];	// of server
+char messageToServer[MESSAGE_TO_SERVER_SIZE];					// of client
+int mode;														//determine server/ client/ nothing
+int characterSelection;											
+char gameStart[2];												// not(N) start(G)
 
-SOCKET hServSock;			// of server
-SOCKET hClntSock[6];		// of server
-SOCKET hSocket;				// of client
-SOCKADDR_IN servAddr;		// of server/client
-SOCKADDR_IN clntAddr[6];	// of server
-int szClntAddr[6];			// of server
-char messageToClient[3];		// of server
-char messageFromClient[6][16]; // of server
-char messageToServer[];		// of client
-int mode;					//determine server(1) client(2) or nothing (0)
-int characterSelection;		//character_selection_time;
-char gameStart[2];				// start(1)
+Unit unitArray[UNIT_NUM_MAX] = {Unit(TEAM_POSTECH), Unit(TEAM_POSTECH), Unit(TEAM_POSTECH), Unit(TEAM_KAIST), Unit(TEAM_KAIST), Unit(TEAM_KAIST)};
+Flag flagArray[FLAG_NUM_MAX] = {Flag(FLAG1_X, FLAG1_Y), Flag(FLAG2_X, FLAG2_Y) , Flag(FLAG3_X, FLAG3_Y) , Flag(FLAG4_X, FLAG4_Y) , Flag(FLAG5_X, FLAG5_Y) };
+//Poison poisonArray[POISON_NUM_MAX];
+//Petal petalArray[PETAL_NUM_MAX];
+//Mushroom mushroomArray[MUSHROOM_NUM_MAX];
 
+int score[2];
+int turnleft;
 
 
 void main(int argc, char **argv)
 {
 	srand(time(NULL));
-	printf("Hi");
 
-	mode = 0;
+	mode = MODE_NOTHING;
 	characterSelection = 0;
-
-	messageToClient[0] = 'h';
-	messageToClient[1] = 'i';
-	messageToClient[2] = '\0';
-
 	gameStart[0] = 'N';
 	gameStart[1] = '\0';
 
@@ -58,20 +60,10 @@ void main(int argc, char **argv)
 	glutCreateWindow("CNH - Network Project");
 
 	glewInit();
-
 	Shader::init();
 	//60fps, 0.5 second per turn
 	Timer::init(16, 30);
 	Key::init();
-
-	/*if (argc == 2) mode = 1;
-	else if (argc == 3) mode = 2;
-	else
-	{
-		printf("Server Usage : %s <port> \n", argv[0]);
-		printf("Client Usage : %s <ip> <port>\n", argv[0]);
-		exit(1);
-	}*/
 
 	glutMainLoop();
 }
--- NetworkProject/timer.cpp
+++ NetworkProject/timer.cpp
@@ -15,6 +15,7 @@
 #include "server.h"
 #include "client.h"
 #include "protocol.h"
+#include "unit.h"
 #include "key.h"
 
 using namespace std;
@@ -34,9 +35,7 @@ extern char messageToServer[];		// of client
 extern int mode;					//determine server(1) client(2) or nothing (0)
 extern int characterSelection;		//character_selection_time;
 extern char gameStart[2];				// start(1)
-
-
-
+extern Unit unitArray[UNIT_NUM_MAX];
 
 int command;
 
@@ -50,21 +49,19 @@ void Timer::init(int interval, int perturn) {
 
 void Timer::turn() {
 	//std::cout << "Turn!" << std::endl;
-
 	// add any per turn action
 
-	if (mode == 8 && gameStart[0] == 'G')
+	if (mode == MODE_CLIENT && gameStart[0] == GAME_START_CHAR)
 	{
 		sendToServer(&hSocket, (char *)(to_string(command)).c_str());
-		printf("command : %d was sent", command);
+		printf("command : %d was sent\n", command);
 	}
-	if (mode == 9 && gameStart[0] != 'G')
+	if (mode == MODE_SERVER && gameStart[0] != GAME_START_CHAR)
 	{
-		printf("GAME START!!!!!!!!!!!!!!!!");
+		printf("GAME START!!!!!!!!!!!!!!!!\n");
 		sendToClient(messageToClient, hClntSock);
-
 		recieveFromClient(hClntSock, messageFromClient);	// recieve data;
-		printf("%s", messageFromClient);
+		printf("%s\n", messageFromClient);
 	}
 }
 
@@ -75,92 +72,98 @@ void Timer::update(int count) {
 		turn();
 	}
 
-	//if (mode > 0) printf("mode CHANGED!");
-
 	// add any per frame actions
 	// such as update() and draw() for all object
 
-	if (mode == 0 && Key::keyCheckPressed('9'))
+	if (mode == MODE_NOTHING && Key::keyCheckPressed(MODE_SERVER_KEY))
 	{
-		mode = 9;
+		char gameStartMessage[2];
+		gameStartMessage[0] = GAME_START_CHAR;
+		gameStartMessage[1] = '\0';
+
+		mode = MODE_SERVER;
 		printf("mode- server chosn\n");
 		makeServerSocket(&hServSock, &servAddr);
-		printf("socket made");
 		acceptClient(&hServSock, hClntSock, clntAddr, szClntAddr);
-		recieveFromClient(hClntSock, messageFromClient);	// recieve spawn 
+		recieveFromClient(hClntSock, messageFromClient);			// recieve spawn
+		printf("SELECTED : \n");
+		
+		for (int i = 0; i < UNIT_NUM_MAX; i++)
+		{
+			if (unitArray[i].getTeam() == TEAM_POSTECH)
+			{
+				unitArray[i].spawn(TEAM_POSTECH_SPAWN_X, TEAM_POSTECH_SPAWN_Y, (protocol_dep)(atoi(messageFromClient[i])));
+				printf("%d! ", atoi(messageFromClient[i]));
+			}
+			else if ((unitArray[i].getTeam() == TEAM_KAIST))
+			{
+				unitArray[i].spawn(TEAM_KAIST_SPAWN_X, TEAM_KAIST_SPAWN_Y, (protocol_dep)(atoi(messageFromClient[i])));
+				printf("%d! ", atoi(messageFromClient[i]));
+			}
+		}
 
-		char acceptMessage[2];
-		acceptMessage[0] = 'G';
-		acceptMessage[1] = '\0';
-		sendToClient(acceptMessage, hClntSock);				// accept clients
-
-		gameStart[0] = 'G';									// game started
+		sendToClient(gameStartMessage, hClntSock);					// accept clients
+		gameStart[0] = GAME_START_CHAR;								// game started
 	}
 
-	if (mode == 0 && Key::keyCheckPressed('8'))
+	if (mode == MODE_NOTHING && Key::keyCheckPressed(MODE_CLIENT_KEY))
 	{
-		mode = 8;
+		mode = MODE_CLIENT;
 		printf("mode- client chose\n");
 		makeClientSocket(&hSocket, &servAddr);
 		connectToServer(&servAddr, &hSocket);
-	}// if not 1 or 2 give error message
-
-	//if (characterSelection == 0 && Key::keyCheckPressed('a') && Key::keyCheckOn('s')) printf("hihi"); // sa
+	}// if invalid input, we should give error message
 	
-	if (mode > 0)
+	if (mode == MODE_SERVER || mode == MODE_CLIENT)					// 
 	{
-		if (mode==8 && characterSelection == 0 && Key::keyCheckPressed('1'))
+		if (mode==MODE_CLIENT && characterSelection == 0 && Key::keyCheckPressed('1'))
 		{
-			characterSelection = COMMAND_SPAWN_CSE;
+			characterSelection = DEP_CSE;
 			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
 			printf("character chosen : %d", characterSelection);
 			getProtocolDataFromServer(&hSocket, gameStart);
 			printf("GameStart!");
 		}
-		if (mode == 8 && characterSelection == 0 && Key::keyCheckPressed('2'))
+		if (mode == MODE_CLIENT && characterSelection == 0 && Key::keyCheckPressed('2'))
 		{
-			characterSelection = COMMAND_SPAWN_PHYS;
+			characterSelection = DEP_PHYS;
 			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
 			printf("character chosen : %d", characterSelection);
 			getProtocolDataFromServer(&hSocket, gameStart);
 			printf("GameStart!");
 		}
-		if (mode == 8 && characterSelection == 0 && Key::keyCheckPressed('3'))
+		if (mode == MODE_CLIENT && characterSelection == 0 && Key::keyCheckPressed('3'))
 		{
-			characterSelection = COMMAND_SPAWN_LIFE;
-			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
-			printf("character chosen : %d", characterSelection);
-			getProtocolDataFromServer(&hSocket, gameStart);
-			printf("GameStart!");
-		}
-
-		if (mode == 8 && characterSelection == 0 && Key::keyCheckPressed('4'))
-		{
-			characterSelection = COMMAND_SPAWN_ME;
+			characterSelection = DEP_LIFE;
 			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
 			printf("character chosen : %d", characterSelection);
 			getProtocolDataFromServer(&hSocket, gameStart);
 			printf("GameStart!");
 		}
 
-		if (mode == 8 && characterSelection == 0 && Key::keyCheckPressed('5'))
+		if (mode == MODE_CLIENT && characterSelection == 0 && Key::keyCheckPressed('4'))
 		{
-			characterSelection = COMMAND_SPAWN_CHEM;
+			characterSelection = DEP_ME;
 			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
 			printf("character chosen : %d", characterSelection);
 			getProtocolDataFromServer(&hSocket, gameStart);
 			printf("GameStart!");
 		}
 
-		if (mode == 8 && characterSelection > 0 && gameStart[0] == 'G')
+		if (mode == MODE_CLIENT && characterSelection == 0 && Key::keyCheckPressed('5'))
+		{
+			characterSelection = DEP_CHEM;
+			sendToServer(&hSocket, (char *)(to_string(characterSelection)).c_str());
+			printf("character chosen : %d\n", characterSelection);
+			getProtocolDataFromServer(&hSocket, gameStart);
+			printf("GameStart!\n");
+		}
+
+		if (mode == MODE_CLIENT && characterSelection > 0 && gameStart[0] == GAME_START_CHAR)
 		{
 			if (Key::keyCheckPressed('d')) command = COMMAND_MOVE_RIGHT;
 			if (Key::keyCheckPressed('w')) command = COMMAND_MOVE_UP;
-			if (Key::keyCheckPressed('a'))
-			{
-				command = COMMAND_MOVE_LEFT;
-		
-			}
+			if (Key::keyCheckPressed('a')) command = COMMAND_MOVE_LEFT;
 			if (Key::keyCheckPressed('s')) command = COMMAND_MOVE_DOWN;
 
 			if (Key::keyCheckPressed('d') && Key::keyCheckOn('j')) command = COMMAND_ATTACK_RIGHT;
